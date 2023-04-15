8ec18bedb298f72b6230262174ba7fcc7775ac63
Juan Esteban Caicedo Pardo <2220201024@estudiantesunibague.edu.co>
2023-04-14 19:38:08 -0500

    Clase cleaner con métodos para tratamiento de corpus y corpus chat.txt

--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from cleaner import clean_corpus
+
+
+CORPUS_FILE = "chat.txt"
 
 chatbot = ChatBot("Chatpot")
 
 trainer = ListTrainer(chatbot)
-trainer.train([
-    "Hola",
-    "Hola Amigo"
-])
-trainer.train([
-    "¿Eres una planta?",
-    "No, soy un bot"
-])
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+trainer.train(cleaned_corpus)
+print(cleaned_corpus)
 
 exit_conditions = (":q", "quit", "exit")
 while True:
